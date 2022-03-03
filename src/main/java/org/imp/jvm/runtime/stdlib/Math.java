91f9af9bf5dbc9c38a10ae12e15b07803d6a90c6
omni-works <83828882+omnit3a@users.noreply.github.com>
2022-03-03 14:27:35 -0500

    Add seeded random number generation functions (#30)

--- src/main/java/org/imp/jvm/runtime/stdlib/Math.java
+++ src/main/java/org/imp/jvm/runtime/stdlib/Math.java
@@ -12,9 +12,19 @@ public class Math {
         Random rand = new Random();
         return rand.nextFloat();
     }
+    
+    public static float rand(int seed) {
+        Random rand = new Random(seed);
+        return rand.nextFloat();
+    }
 
     public static float rand(int start, int end) {
         Random rand = new Random();
+        return rand.nextInt(start, end);
+    }
+    
+    public static float rand(int start, int end, int seed){
+        Random rand = new Random(seed);
         return rand.nextInt(start, end);
     }
 
