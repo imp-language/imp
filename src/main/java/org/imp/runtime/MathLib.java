2c5a80fddccb3fb672df1ba35b149009968855e2
omni-works <83828882+omnit3a@users.noreply.github.com>
2022-03-04 17:03:54 -0500

    Create MathLib.java

---
+++ src/main/java/org/imp/runtime/MathLib.java
@@ -0,0 +1,11 @@
+package org.imp.runtime;
+
+import java.lang.String;
+
+public class MathLib {
+
+    //this method is simply for testing, and should be removed after the Math stdlib is verified to work
+    public static String intToString(int a){
+        return String.valueOf(a);
+    }
+}
