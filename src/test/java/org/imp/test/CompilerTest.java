e818217fe85788db69a5f73f9c9979f5b7600ee7
omni-works <83828882+omnit3a@users.noreply.github.com>
2022-03-04 17:41:34 -0500

    Update CompilerTest.java

--- src/test/java/org/imp/test/CompilerTest.java
+++ src/test/java/org/imp/test/CompilerTest.java
@@ -30,7 +30,6 @@ public class CompilerTest {
     @Test
     void StdMath() throws IOException, InterruptedException {
         assertEquals(Load.run("stdlib/MathLibTest", moduleLocation), """
-                MathLibTest
                 5
                 """);
     }
