73a08f97ab91a34b467ac0164430fa7b9b73ade3
omni-works <83828882+omnit3a@users.noreply.github.com>
2022-03-04 17:19:19 -0500

    Add testing method

--- src/main/java/org/imp/runtime/MathLib.java
+++ src/main/java/org/imp/runtime/MathLib.java
@@ -5,7 +5,7 @@ import java.lang.String;
 public class MathLib {
 
     //this method is simply for testing, and should be removed after the Math stdlib is verified to work
-    public static String intToString(int a){
-        return String.valueOf(a);
+    public static int ret(int a){
+        ret a;
     }
 }
