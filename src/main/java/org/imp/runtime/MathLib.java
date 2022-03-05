56b4a01f4f1a0ac96c5c0b8e175716ef70a00d0d
omni-works <83828882+omnit3a@users.noreply.github.com>
2022-03-04 20:41:24 -0500

    Update MathLib.java

--- src/main/java/org/imp/runtime/MathLib.java
+++ src/main/java/org/imp/runtime/MathLib.java
@@ -1,7 +1,5 @@
 package org.imp.runtime;
 
-import java.lang.String;
-
 public class MathLib {
 
     //this method is simply for testing, and should be removed after the Math stdlib is verified to work
