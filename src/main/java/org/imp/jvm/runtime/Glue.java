0857b36940e6195b28b0bd27c6efebdaec874ef4
omni-works <83828882+omnit3a@users.noreply.github.com>
2022-03-04 22:13:03 -0500

    Add EasterEggs.java to core module

--- src/main/java/org/imp/jvm/runtime/Glue.java
+++ src/main/java/org/imp/jvm/runtime/Glue.java
@@ -5,6 +5,7 @@ import org.imp.jvm.legacy.domain.scope.Identifier;
 import org.imp.jvm.legacy.expression.Function;
 import org.imp.jvm.types.*;
 import org.imp.runtime.Batteries;
+import org.imp.runtime.EasterEggs
 
 import java.lang.reflect.Method;
 import java.util.*;
@@ -19,6 +20,7 @@ public class Glue {
 
     static {
         coreModules.put("batteries", Batteries.class);
+        coreModules.put("normalStuff",EasterEggs.class);
     }
 
     public static List<FuncType> getExports(String module) {
