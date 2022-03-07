e83efbeeae8b8864d65c029db97c51bb030d9202
omni-works <83828882+omnit3a@users.noreply.github.com>
2022-03-07 09:24:40 -0500

    Update Batteries.java

--- src/main/java/org/imp/runtime/Batteries.java
+++ src/main/java/org/imp/runtime/Batteries.java
@@ -43,7 +43,7 @@ public class Batteries {
     }
     
     public static String atSpot(String input, int position){
-        if (position < input.length();){
+        if (position < input.length()){
             return input.charAt(position);
         }
         return "null";
