c1f7a145bc31881fd1e649dd9b884c35d9a8f558
omni-works <83828882+omnit3a@users.noreply.github.com>
2022-03-08 23:13:02 -0500

    Remove Ret function

--- src/main/java/org/imp/runtime/MathLib.java
+++ src/main/java/org/imp/runtime/MathLib.java
@@ -2,11 +2,6 @@ package org.imp.runtime;
 
 public class MathLib {
 
-    //this method is simply for testing, and should be removed after the Math stdlib is verified to work
-    public static int ret(int a){
-        return a;
-    }
-
     public static int mod(int a, int b){
         return a % b;
     }
