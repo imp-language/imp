1877c13dc38644bf40245d4f481f712dd44e9e79
omni-works <83828882+omnit3a@users.noreply.github.com>
2022-03-09 15:22:11 -0500

    Create a completely normal file

---
+++ src/main/java/org/imp/runtime/SecretLib.java
@@ -0,0 +1,10 @@
+package org.imp.runtime;
+
+public class SecretLib {
+
+    public static void wait6502(int amount){
+        for (int i = 0 ; i < amount ; i++){
+            System.out.println("MICROSOFT!");
+        }
+    }
+}
