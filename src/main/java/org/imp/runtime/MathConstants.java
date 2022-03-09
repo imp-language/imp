caf546935e2c1d70757453ca4bca876107d8c01a
omni-works <83828882+omnit3a@users.noreply.github.com>
2022-03-09 12:10:34 -0500

    Add getter methods for constants

--- src/main/java/org/imp/runtime/MathConstants.java
+++ src/main/java/org/imp/runtime/MathConstants.java
@@ -3,11 +3,25 @@ package org.imp.runtime;
 public class MathConstants {
 
     //system constants
-    public final int intMax = 2147483647;
-    public final int intMin = -2147483648;
+    public static final int intMax = 2147483647;
+    public static final int intMin = -2147483648;
   
     //mathematical constants
-    public final double pi = 3.14159;
-    public final double phi = 1.61803;
+    public static final double pi = 3.14159;
+    public static final double phi = 1.61803;
+    
+    public static int getMax(){
+        return intMax;
+    }
+    public static int getMin(){
+        return intMin;
+    }
+    
+    public static double pi(){
+        return pi;
+    }
+    public static double phi(){
+        return phi;
+    }
     
 }
