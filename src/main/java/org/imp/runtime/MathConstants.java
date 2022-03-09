1f478c46c9f51468c59da44b7e2ef2ddb1315364
omni-works <83828882+omnit3a@users.noreply.github.com>
2022-03-09 11:59:14 -0500

    Create MathConstants.java

---
+++ src/main/java/org/imp/runtime/MathConstants.java
@@ -0,0 +1,13 @@
+package org.imp.runtime;
+
+public class MathConstants {
+
+    //system constants
+    public final int intMax = 2147483647;
+    public final int intMin = -2147483648;
+  
+    //mathematical constants
+    public final float pi = 3.14159;
+    public final float phi = 1.61803;
+    
+}
