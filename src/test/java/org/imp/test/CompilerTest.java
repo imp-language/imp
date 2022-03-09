73ce0c50c18d7eaab91660f9700c4da42c64cd2c
omni-works <83828882+omnit3a@users.noreply.github.com>
2022-03-09 14:57:29 -0500

    Update CompilerTest.java

--- src/test/java/org/imp/test/CompilerTest.java
+++ src/test/java/org/imp/test/CompilerTest.java
@@ -51,6 +51,8 @@ public class CompilerTest {
         assertEquals(Load.run("stdlib/Constants", moduleLocation), """
                 2147483647
                 -2147483648
+                0
+                1
                 3.14159
                 1.61803
                 """);
