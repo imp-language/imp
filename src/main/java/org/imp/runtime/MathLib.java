d1b8b27559bd4372dd03d2387001340cea52bfc7
omni-works <83828882+omnit3a@users.noreply.github.com>
2022-03-09 09:50:17 -0500

    Add a comment about long ranges constants

--- src/main/java/org/imp/runtime/MathLib.java
+++ src/main/java/org/imp/runtime/MathLib.java
@@ -5,6 +5,7 @@ public class MathLib {
     public static final int intMax = 2147483647;
     public static final int intMin = -2147483648;
     
+    //I am fully aware that there is currently no long datatype, this is simply here so that i dont forget to add the ranges if we do add longs
     public static final long longMax = 9223372036854775807;
     public static final long longMin = -9223372036854775808;
 
