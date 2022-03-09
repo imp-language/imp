7f94bbbc8d4d1ef814bfe3bcd17386928fbe8653
omni-works <83828882+omnit3a@users.noreply.github.com>
2022-03-09 09:46:11 -0500

    Add integer range constants

--- src/main/java/org/imp/runtime/MathLib.java
+++ src/main/java/org/imp/runtime/MathLib.java
@@ -1,6 +1,9 @@
 package org.imp.runtime;
 
 public class MathLib {
+    
+    public static final int intMax = 2147483647;
+    public static final int intMin = -2147483648;
 
     public static int mod(int a, int b){
         return a % b;
