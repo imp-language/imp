13796a0b66d58b198cbcd11063e9b7c39503b70f
omni-works <83828882+omnit3a@users.noreply.github.com>
2022-03-09 09:49:08 -0500

    Add constants for Long range

--- src/main/java/org/imp/runtime/MathLib.java
+++ src/main/java/org/imp/runtime/MathLib.java
@@ -4,6 +4,9 @@ public class MathLib {
     
     public static final int intMax = 2147483647;
     public static final int intMin = -2147483648;
+    
+    public static final long longMax = 9223372036854775807;
+    public static final long longMin = -9223372036854775808;
 
     public static int mod(int a, int b){
         return a % b;
