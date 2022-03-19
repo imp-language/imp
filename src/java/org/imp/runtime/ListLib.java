253f8d723de77e470334d8491ab76f722191b520
Matt Hall <matthew349hall@hotmail.com>
2022-03-19 15:18:24 -0400

    started list test

--- src/java/org/imp/runtime/ListLib.java
+++ src/java/org/imp/runtime/ListLib.java
@@ -1,16 +1,15 @@
 package org.imp.runtime;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class ListLib {
-    public static <T> void push(ArrayList<T> r, T a) {
+    public static <T> void push(List<T> r, T a) {
         r.add(a);
     }
 
-    public static <T> T pop(ArrayList<T> r) {
+    public static <T> T pop(List<T> r) {
         return r.remove(r.size() - 1);
     }
 
@@ -18,5 +17,9 @@ public class ListLib {
         // Todo: this is not performant (generates the entire array ahead of time)
         // When possible, a stream/iterator should be used instead.
         return IntStream.range(start, stop).boxed().collect(Collectors.toList());
+    }
+
+    public static <T> T at(List<T> r, int pos) {
+        return r.get(pos);
     }
 }
